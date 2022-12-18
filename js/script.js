185b926f53315763240f0a8c84538803ed170598
zewdu erkyhun <zewdu444@gmail.com>
2022-12-18 16:09:02 +0300

    Add method and eventlistener

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const mobileoption = document.getElementById("mobileoption");
+const body = document.querySelector("body");
+const content = document.querySelector(".content");
+mobileoption.style.display = "none";
+
+function openOption() {
+  mobileoption.style.display = "block";
+  body.style.overflow = "hidden";
+}
+
+// add event listener to mobile option
+mobileoption.addEventListener("click", function () {
+  mobileoption.style.display = "none";
+  body.style.overflow = "auto";
+});
